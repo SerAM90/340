e01d8f3530b65f771c5f31678bebf4e79e61a903
SerAM90 <97140657+SerAM90@users.noreply.github.com>
2023-06-13 00:25:11 -0400

    Add files via upload

--- update_vendor.js
+++ update_vendor.js
@@ -66,10 +66,10 @@ function updateRow(data, idVendor){
             // Get the location of the row where we found the matching person ID
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
-            // Get td of homeworld value
+            // Get td of vendor value
             let td = updateRowIndex.getElementsByTagName("td")[3];
 
-            // Reassign homeworld to our value we updated to
+            // Reassign vendorAddress to our value we updated to
             td.innerHTML = parsedData[0].vendorAddress; 
        }
     }
